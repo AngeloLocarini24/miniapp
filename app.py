a0074b4fa6116befa753803173c2ace8fea095a0
AngeloLocarini24 <angelolocarini004@gmail.com>
2025-09-24 16:01:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ print_routes()
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=9000)
+    app.run(debug=True, host = "0.0.0.0" , port=9000)
